49f1f1c7fc3fd4ffadf780e5e58ecfecfac3fcdd
Matthijs Kropholler <matthijsk2000@pm.me>
2021-03-23 18:26:58 +0100

    Hide list when fetching data

--- app/src/main/java/com/avans/assessment/ui/components/NavigationAppBar.kt
+++ app/src/main/java/com/avans/assessment/ui/components/NavigationAppBar.kt
@@ -1,13 +1,10 @@
 package com.avans.assessment.ui.components
 
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import kotlinx.coroutines.launch
 
 @Composable
@@ -26,7 +23,6 @@ fun NavigationAppBar(name: String, scaffoldState: ScaffoldState){
                 }
             ) {
                 Icon(Icons.Default.Menu, "Menu button")
-                CircularProgressIndicator(modifier = Modifier.wrapContentWidth(Alignment.CenterHorizontally))
             }
         }
     )
--- app/src/main/java/com/avans/assessment/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/avans/assessment/ui/screens/HomeScreen.kt
@@ -1,12 +1,19 @@
 package com.avans.assessment.ui.screens
 
 import android.content.Context
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
 import com.avans.assessment.models.Beer
 import com.avans.assessment.viewmodels.BeerListViewModel
 
@@ -17,9 +24,19 @@ fun HomeScreen(context: Context){
 
     beerListViewModel.loadBeers()
 
-    LazyColumn {
-        items(beers) { beer ->
-            Text(text = beer.name)
+    if (beers.isEmpty()) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = CenterHorizontally
+        ) {
+            CircularProgressIndicator(modifier = Modifier.wrapContentWidth(CenterHorizontally))
+        }
+    } else {
+        LazyColumn {
+            items(beers) { beer ->
+                Text(text = beer.name)
+            }
         }
     }
 }
