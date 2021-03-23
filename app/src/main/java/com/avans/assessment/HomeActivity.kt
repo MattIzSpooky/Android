0162a6373dcc9f9bb4237f08043577dded7e7860
Matthijs Kropholler <matthijsk2000@pm.me>
2021-03-23 16:04:04 +0100

    Add test code

--- app/src/main/java/com/avans/assessment/HomeActivity.kt
+++ app/src/main/java/com/avans/assessment/HomeActivity.kt
@@ -3,21 +3,71 @@ package com.avans.assessment
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigate
+import androidx.navigation.compose.rememberNavController
 import com.avans.assessment.ui.theme.AvansandroidassessmentTheme
+import kotlinx.coroutines.launch
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val testViewModel = TestViewModel(this.applicationContext)
+        
         setContent {
             AvansandroidassessmentTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    val scaffoldState = rememberScaffoldState()
+                    val scope = rememberCoroutineScope()
+                    val navController = rememberNavController()
+                    Scaffold(
+                        scaffoldState = scaffoldState,
+                        topBar = {
+                            TopAppBar(
+                                title = { Text(text = "AppBar") },
+                                navigationIcon = {
+                                    IconButton(
+                                        onClick = {
+                                            scope.launch { if (scaffoldState.drawerState.isOpen) scaffoldState.drawerState.close() else scaffoldState.drawerState.open() }
+                                        }
+                                    ) {
+                                        Icon(Icons.Default.Menu, "Menu button")
+                                    }
+                                }
+                            )
+                        },
+                        drawerShape = RoundedCornerShape(10.dp, 10.dp),
+                        drawerContent = {
+                            Button(onClick = { navController.navigate("home") }) {
+                                Text(text = "Home")
+                            }
+
+                            Button(onClick = { navController.navigate("favorites") }) {
+                                Text(text = "Favorites")
+                            }
+                        }
+                    ) {
+                        NavHost(navController, startDestination = "home") {
+                            composable("home") { NewsStory(navController = navController) }
+                            composable("favorites") { Test(testViewModel = testViewModel, action = { testViewModel.loadData() }) }
+                        }
+                    }
                 }
             }
         }
@@ -25,14 +75,58 @@ class HomeActivity : AppCompatActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Test(testViewModel: TestViewModel, action: () -> Unit) {
+    val name: String by testViewModel.data.observeAsState(initial = "")
+    
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = action) {
+            Text("Fetch me")
+        }
+
+        if (name.isEmpty()) {
+            CircularProgressIndicator(modifier = Modifier.wrapContentWidth(Alignment.CenterHorizontally))
+        } else {
+            Text(text = name)
+        }
+    }
 }
+
+@Composable
+fun NewsStory(navController: NavController) {
+    MaterialTheme {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                "A day wandering through the sandhills " +
+                        "in Shark Fin Cove, and a few of the " +
+                        "sights I saw",
+                style =  MaterialTheme.typography.h6,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(
+                "Davenport, California",
+                style =  MaterialTheme.typography.body2
+            )
+            Text(
+                "December 2018",
+                style =  MaterialTheme.typography.body2
+            )
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    AvansandroidassessmentTheme {
-        Greeting("Android")
-    }
+    HomeActivity()
+//    ComposeTutorialTheme {
+//        NewsStory()
+//    }
 }
---
+++ app/src/main/java/com/avans/assessment/TestViewModel.kt
@@ -0,0 +1,26 @@
+package com.avans.assessment
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import java.lang.ref.WeakReference
+
+class TestViewModel(ctx: Context) : ViewModel() {
+    private val _data = MutableLiveData("")
+    val data: LiveData<String> = _data
+
+    private val context = WeakReference(ctx)
+
+    fun loadData() {
+        val stringRequest = StringRequest(
+            Request.Method.GET, "https://www.google.com/",
+            { response ->
+                this._data.value = response
+            },
+            { print("kaput") })
+        this.context.get()?.let { ApiClient.getInstance(it).addToRequestQueue(stringRequest) }
+    }
+}
